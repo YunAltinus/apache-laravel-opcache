28318953a7a4a475d1539cb0de35c819e5f7f247
Yunus ALTIN <yunusaltin1963@gmail.com>
2024-07-17 23:53:06 +0300

    first commit

--- www/config/horizon.php
+++ www/config/horizon.php
@@ -193,6 +193,19 @@ return [
             'timeout' => 60,
             'nice' => 0,
         ],
+        'supervisor-2' => [
+            'connection' => 'redis',
+            'queue' => ['default'],
+            'balance' => 'auto',
+            'autoScalingStrategy' => 'time',
+            'maxProcesses' => 1,
+            'maxTime' => 0,
+            'maxJobs' => 0,
+            'memory' => 128,
+            'tries' => 1,
+            'timeout' => 60,
+            'nice' => 0,
+        ],
     ],
 
     'environments' => [
@@ -206,6 +219,9 @@ return [
 
         'local' => [
             'supervisor-1' => [
+                'maxProcesses' => 3,
+            ],
+            'supervisor-2' => [
                 'maxProcesses' => 3,
             ],
         ],
